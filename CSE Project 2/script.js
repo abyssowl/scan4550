072292c5ac7afe00afba4cdde941e80696bccd8e
dannysan66 <34259485+dannysan66@users.noreply.github.com>
2021-10-25 20:48:04 -0700

    Delete CSE Project 2 directory

--- CSE Project 2/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementByClassName('toggle-button')[0]
-const navbarLinks = document.getElementByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
